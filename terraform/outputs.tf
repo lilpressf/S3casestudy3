13bcd20137069a411c07fdcf9a5b2ef10b973545
lilpressf <daanwelten05@gmail.com>
2025-11-27 10:00:01 +0100

    fixed outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,10 +13,6 @@ output "private_subnet_ids" {
   description = "Private subnet IDs"
 }
 
-output "nat_public_ip" {
-  value       = aws_instance.nat.public_ip
-  description = "Public IP of the NAT instance"
-}
 
 output "eks_cluster_name" {
   value       = aws_eks_cluster.main.name
