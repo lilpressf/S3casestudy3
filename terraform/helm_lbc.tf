ede2c5200ada957e15d24581f1b64fec59460ef1
lilpressf <daanwelten05@gmail.com>
2025-12-03 17:46:26 +0100

    helm stuff

--- terraform/helm_lbc.tf
+++ terraform/helm_lbc.tf
@@ -1,13 +1,24 @@
 # AWS Load Balancer Controller via Helm   
+# AWS Load Balancer Controller via Helm
 provider "helm" {
+  alias = "eks"
+
   kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--region", var.aws_region, "--cluster-name", aws_eks_cluster.main.name]
+    }
   }
 }
 
 resource "helm_release" "aws_load_balancer_controller" {
+  provider = helm.eks
+
   name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
@@ -33,20 +44,3 @@ resource "helm_release" "aws_load_balancer_controller" {
     name  = "serviceAccount.create"
     value = "true"
   }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.lbc_irsa.arn
-  }
-
-  depends_on = [
-    aws_eks_cluster.main,
-    aws_iam_role_policy.lbc_inline,
-    aws_eks_node_group.default
-  ]
-}
