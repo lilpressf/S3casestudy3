584950cbea7ab347aac3c12e2f1d56bc1ab44768
lilpressf <daanwelten05@gmail.com>
2025-12-03 17:43:42 +0100

    helm stuff

--- terraform/helm_lbc.tf
+++ terraform/helm_lbc.tf
@@ -1,8 +1,8 @@
 # AWS Load Balancer Controller via Helm   
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
