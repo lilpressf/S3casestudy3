6d7acf813f297a8d03051d91c6ac89e4f0903c37
lilpressf <daanwelten05@gmail.com>
2025-12-03 18:02:18 +0100

    helm stuff

--- terraform/helm_lbc.tf
+++ terraform/helm_lbc.tf
@@ -3,7 +3,11 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--region", var.aws_region, "--cluster-name", aws_eks_cluster.main.name]
+    }
   }
 }
 
