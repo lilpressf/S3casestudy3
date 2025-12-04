f1df63264215dad60bf31a6026fdf45de00dfd6a
lilpressf <daanwelten05@gmail.com>
2025-12-04 14:40:57 +0100

    dsfdsfdsa

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,64 +1,62 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-central-1"
+output "vpc_id" {
+  value       = aws_vpc.main.id
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
 }
 
-variable "public_subnet_a_cidr" {
-  description = "CIDR block for public subnet A"
-  type        = string
-  default     = "10.0.1.0/24"
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
 }
 
-variable "public_subnet_b_cidr" {
-  description = "CIDR block for public subnet B"
-  type        = string
-  default     = "10.0.2.0/24"
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
 }
 
-variable "private_subnet_a_cidr" {
-  description = "CIDR block for private subnet A"
-  type        = string
-  default     = "10.0.3.0/24"
+output "eks_node_group_name" {
+  value = aws_eks_node_group.default.node_group_name
 }
 
-variable "private_subnet_b_cidr" {
-  description = "CIDR block for private subnet B"
-  type        = string
-  default     = "10.0.4.0/24"
+output "eks_node_role_arn" {
+  value = aws_iam_role.eks_node_role.arn
 }
 
-variable "ssh_public_key" {
-  description = "SSH public key contents"
-  type        = string
-  sensitive   = true
+output "lbc_irsa_role_arn" {
+  value = aws_iam_role.lbc_irsa.arn
 }
 
-variable "ssh_cidr" {
-  description = "CIDR block allowed SSH access"
-  type        = string
+output "backend_irsa_role_arn" {
+  value = aws_iam_role.backend_irsa.arn
 }
 
-variable "acm_certificate_arn" {
-  description = "ACM certificate ARN for ALB"
-  type        = string
-  default     = ""
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.portal.id
 }
 
-variable "workspaces_directory_id" {
-  description = "AWS WorkSpaces Directory ID"
-  type        = string
-  default     = ""
+output "cognito_user_pool_client_id" {
+  value = aws_cognito_user_pool_client.portal.id
 }
 
-variable "workspaces_bundle_id" {
-  description = "AWS WorkSpaces Bundle ID"
-  type        = string
-  default     = ""
+output "cognito_user_pool_client_secret" {
+  value = aws_cognito_user_pool_client.portal.client_secret
+  sensitive = true
+}
+
+output "cognito_user_pool_domain" {
+  value = aws_cognito_user_pool_domain.portal.domain
+}
+
+output "cognito_user_pool_arn" {
+  value = aws_cognito_user_pool.portal.arn
+}
+
+output "acm_certificate_arn" {
+  value = aws_acm_certificate.portal.arn
 }
