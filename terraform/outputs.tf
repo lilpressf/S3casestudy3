6e7cb8e22f50a113639a18f6752127acb9ffbc8c
lilpressf <daanwelten05@gmail.com>
2025-12-10 10:49:32 +0100

    fdsfdsfs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
+###############################################
+# Networking
+###############################################
 output "vpc_id" {
   value = aws_vpc.main.id
 }
@@ -16,18 +19,24 @@ output "private_subnet_ids" {
   ]
 }
 
+###############################################
+# EKS
+###############################################
 output "eks_cluster_name" {
   value = aws_eks_cluster.main.name
 }
 
 output "eks_node_group_name" {
-  value = aws_eks_node_group.default.node_group_name
+  value = aws_eks_node_group.main.node_group_name
 }
 
 output "eks_node_role_arn" {
   value = aws_iam_role.eks_node_role.arn
 }
 
+###############################################
+# IAM (IRSA)
+###############################################
 output "lbc_irsa_role_arn" {
   value = aws_iam_role.lbc_irsa.arn
 }
@@ -36,17 +45,32 @@ output "backend_irsa_role_arn" {
   value = aws_iam_role.backend_irsa.arn
 }
 
-# Cognito values from data sources (read‑only)
+###############################################
+# Cognito – required for GitHub Actions workflow
+###############################################
 output "cognito_user_pool_id" {
-  value = data.aws_cognito_user_pool.portal.id
+  value = aws_cognito_user_pool.portal.id
 }
 
 output "cognito_user_pool_client_id" {
-  value = data.aws_cognito_user_pool_client.portal.id
+  value = aws_cognito_user_pool_client.portal_client.id
 }
 
-# Optional: Cognito domain if you also add a data source for it
-# output "cognito_user_pool_domain" {
-#   value = "innovatech-b37ae2b2"
-# }
+output "cognito_user_pool_client_secret" {
+  value = aws_cognito_user_pool_client.portal_client.client_secret
+}
 
+output "cognito_user_pool_domain" {
+  value = aws_cognito_user_pool_domain.portal_domain.domain
+}
+
+output "cognito_user_pool_arn" {
+  value = aws_cognito_user_pool.portal.arn
+}
+
+###############################################
+# ACM certificate
+###############################################
+output "acm_certificate_arn" {
+  value = aws_acm_certificate_validation.portal.certificate_arn
+}
